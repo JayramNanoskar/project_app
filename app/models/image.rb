f7cca3199865d22b52d8fcd0c2d2e8d15f684175
JayramNanoskar <jayram.nanoskar@kiprosh.com>
2018-06-22 15:29:32 +0530

    validate image fields

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,10 +5,13 @@ class Image < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validate :picture_size
 
+  validates_presence_of :name
+  validates_presence_of :picture
+
   private
 
   def picture_size
-    if picture.size > 5.megabytes
+    if picture.size > 5.kilobytes
       errors.add(:picture,  "should be less than 5MB")
     end
   end
